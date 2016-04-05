faac503b887fb64f7faf2207a0f02f6e1069a98c
Mary <mary_lin@hotmail.co.uk>
2016-04-05 16:58:41 +0100

    moved addnames to function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cgi
 import datetime
 import urllib
@@ -9,11 +10,11 @@ import webapp2
 
 import os
 from google.appengine.ext.webapp import template
-from oauth2client.client import flow_from_clientsecrets
+# from oauth2client.client import flow_from_clientsecrets
 
-flow = flow_from_clientsecrets(os.path.join(os.path.dirname(__file__), 'client_secrets.json'),
-                            scope='https://www.googleapis.com/auth/userinfo.email',
-                            redirect_uri='http:localhost:8080')
+# flow = flow_from_clientsecrets(os.path.join(os.path.dirname(__file__), 'client_secrets.json'),
+#                             scope='https://www.googleapis.com/auth/userinfo.email',
+#                             redirect_uri='http:localhost:8080')
 
 # try:
 #     import googleclouddebugger
@@ -26,13 +27,61 @@ class Ghostname(db.Model):
 
     """Models Ghostname with creator and which user has taken it, if any."""
     ghostname = db.StringProperty(multiline=True)
-    creator = db.UserProperty()
-    taken_by = db.UserProperty()
+    creator = db.StringProperty()
+    taken_by = db.StringProperty()
 
 
 class User(db.Model):
     username = db.UserProperty()
     is_admin = db.BooleanProperty()
+
+
+def addInitialNames():
+    g1 = Ghostname(key_name='Betelgeuse', creator='Default', ghostname='Betelgeuse')
+    g2 = Ghostname(key_name='Bhoot', creator='Default', ghostname='Bhoot')
+    g3 = Ghostname(key_name='Bloody Mary', creator='Default', ghostname='Bloody Mary')
+    g4 = Ghostname(key_name='Bogle', creator='Default', ghostname='Bogle')
+    g5 = Ghostname(key_name='Casper', creator='Default', ghostname='Casper')
+    g6 = Ghostname(key_name='Chindi', creator='Default', ghostname='Chindi')
+    g7 = Ghostname(key_name='Cihuateteo', creator='Default', ghostname='Cihuateteo')
+    g8 = Ghostname(key_name='Clytemnestra', creator='Default', ghostname='Clytemnestra')
+    g9 = Ghostname(key_name='Draugr', creator='Default', ghostname='Draugr')
+    g10 = Ghostname(key_name='Dybbuk', creator='Default', ghostname='Dybbuk')
+    g11 = Ghostname(key_name='Gjenganger', creator='Default', ghostname='Gjenganger')
+    g12 = Ghostname(key_name=u'Guĭ', creator='Default', ghostname=u'Guĭ')
+    g13 = Ghostname(key_name='Ibbur', creator='Default', ghostname='Ibbur')
+    g14 = Ghostname(key_name='Jima', creator='Default', ghostname='Jima')
+    g15 = Ghostname(key_name='Jinn', creator='Default', ghostname='Jinn')
+    g16 = Ghostname(key_name='La Llorona', creator='Default', ghostname='La Llorona')
+    g17 = Ghostname(key_name='Moaning Myrtle', creator='Default', ghostname='Moaning Myrtle')
+    g18 = Ghostname(key_name='Mr. Boogedy', creator='Default', ghostname='Mr. Boogedy')
+    g19 = Ghostname(key_name='Nachzehrer', creator='Default', ghostname='Nachzehrer')
+    g20 = Ghostname(key_name='Blinky', creator='Default', ghostname='Blinky')
+    g21 = Ghostname(key_name='Pinky', creator='Default', ghostname='Pinky')
+    g22 = Ghostname(key_name='Inky', creator='Default', ghostname='Inky')
+    g23 = Ghostname(key_name='Clyde', creator='Default', ghostname='Clyde')
+    g24 = Ghostname(key_name='Patrick Swayze', creator='Default', ghostname='Patrick Swayze')
+    g25 = Ghostname(key_name='Phi Tai Hong', creator='Default', ghostname='Phi Tai Hong')
+    g26 = Ghostname(key_name='Pishacha', creator='Default', ghostname='Pishacha')
+    g27 = Ghostname(key_name='Poltergeist', creator='Default', ghostname='Poltergeist')
+    g28 = Ghostname(key_name='Revenant', creator='Default', ghostname='Revenant')
+    g29 = Ghostname(key_name='Ringwraith', creator='Default', ghostname='Ringwraith')
+    g30 = Ghostname(key_name='Slender Man', creator='Default', ghostname='Slender Man')
+    g31 = Ghostname(key_name='Slimer', creator='Default', ghostname='Slimer')
+    g32 = Ghostname(key_name='Space Ghost', creator='Default', ghostname='Space Ghost')
+    g33 = Ghostname(key_name='Strigoi', creator='Default', ghostname='Strigoi')
+    g34 = Ghostname(key_name='Candyman', creator='Default', ghostname='Candyman')
+    g35 = Ghostname(key_name='The Crypt Keeper', creator='Default', ghostname='The Crypt Keeper')
+    g36 = Ghostname(key_name='Headless Horseman', creator='Default', ghostname='Headless Horseman')
+    g37 = Ghostname(key_name=u'Tomás', creator='Default', ghostname=u'Tomás')
+    g38 = Ghostname(key_name='Vetala', creator='Default', ghostname='Vetala')
+    g39 = Ghostname(key_name=u'Wiedergänger', creator='Default', ghostname=u'Wiedergänger')
+    g40 = Ghostname(key_name='Xunantunich', creator='Default', ghostname='Xunantunich')
+    g41 = Ghostname(key_name=u'Yūrei', creator='Default', ghostname=u'Yūrei')
+    g42 = Ghostname(key_name='Zhong Kui', creator='Default', ghostname='Zhong Kui')
+    g43 = Ghostname(key_name='Zuul', creator='Default', ghostname='Zuul')
+
+    db.put([g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20, g21, g22, g23, g24, g25, g26, g27, g28, g29, g30, g31, g32, g33, g34, g35, g36, g37, g38, g39, g40, g41, g42, g43])
 
 
 class MainPage(webapp2.RequestHandler):
@@ -49,6 +98,9 @@ class MainPage(webapp2.RequestHandler):
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
+
+        addInitialNames()
+
         ghostnames = db.GqlQuery(
             "SELECT * FROM Ghostname ORDER BY ghostname ASC")
 
@@ -124,7 +176,7 @@ def main():
       googleclouddebugger.AttachDebugger()
     except ImportError:
       pass
-        app.RUN()
+    app.RUN()
 
 
 if __name__ == '__main__':
